c3a4fd5bf0eddabf22c7fe1c0cde7ac656f05001
巧克力那么巧 <15700734908@163.com>
2017-12-09 22:45:19 +0800

    singletonDemo6.使用枚举的方法实现单例模式

---
+++ src/com/ysf/singletonDemo6.java
@@ -0,0 +1,13 @@
+package com.ysf;
+
+/**
+ * Create by Tars on 2017/12/9
+ *
+ * 使用枚举类来实现单例模式
+ */
+public enum singletonDemo6 {
+    INSTANCE;
+    //这个方法命随便取
+    public void getInstance() {
+    }
+}
