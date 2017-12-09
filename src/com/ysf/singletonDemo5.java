b6960acdcc68b0949837fa265fc8b6e93740870b
巧克力那么巧 <15700734908@163.com>
2017-12-09 22:47:32 +0800

    singletonDemo6.使用枚举的方法实现单例模式

--- src/com/ysf/singletonDemo5.java
+++ src/com/ysf/singletonDemo5.java
@@ -4,6 +4,9 @@ package com.ysf;
  * Create by Tars on 2017/12/9
  */
 public class singletonDemo5 {
+    /**
+     * 饿汉的另一种模式
+     */
     private static singletonDemo5 instance = new singletonDemo5();
 
     private singletonDemo5() {}
