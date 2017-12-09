b1a8f57532ca97533bfe88f25fe404dea446ea6b
巧克力那么巧 <15700734908@163.com>
2017-12-09 22:32:29 +0800

    singleton5,饿汉的另一种方式

---
+++ src/com/ysf/singletonDemo5.java
@@ -0,0 +1,17 @@
+package com.ysf;
+
+/**
+ * Create by Tars on 2017/12/9
+ */
+public class singletonDemo5 {
+    private static singletonDemo5 instance = new singletonDemo5();
+
+    private singletonDemo5() {}
+
+    public static singletonDemo5 getInstance() {
+        if(instance == null) {
+            instance = new singletonDemo5();
+        }
+        return instance;
+    }
+}
