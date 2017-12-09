c7204375f450df3c6ac19c46945cda72e6c71e41
巧克力那么巧 <15700734908@163.com>
2017-12-09 22:53:20 +0800

    Hello World

---
+++ src/com/ysf/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.ysf;
+
+/**
+ * Create by Tars on 2017/12/9
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
